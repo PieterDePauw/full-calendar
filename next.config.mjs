9e7a97b9c017221fd8ad04bb94bc161814691944
Pieter <pieter@pieterdepauw.com>
2025-03-31 21:02:08 +0200

    refactor: correct allowedDevOrigins IP address in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 export const nextConfig = {
     reactStrictMode: true,
-    allowedDevOrigins: ["192.168.0.37d"],
+    allowedDevOrigins: ["192.168.0.37"],
     images: { unoptimized: true },
     experimental: {
         webpackBuildWorker: true,
